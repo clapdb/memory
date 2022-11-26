17e298be2c26f040b4f08ba285beccdc57e6c42e
hurricane1026 <hurricane1026@gmail.com>
2022-11-26 22:39:45 +0800

    use macro to wrap STDB_ASSERT and assert, if link to libbacktrace, use it.

---
+++ assert/assert_config.hpp
@@ -0,0 +1,12 @@
+
+#if HAVE_BACKTRACE
+#include "hilbert/assert.hpp"
+#define Assert(msg) STDB_ASSERT(msg)
+#define AssertMsg(expr, msg) STDB_ASSERT_MSG(expr, msg)
+
+#else
+
+#include <cassert>
+#define Assert(expr) assert(expr)
+#define AssertMsg(expr, msg) assert((expr) && msg)
+#endif
