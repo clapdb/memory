aab20d799009de499ba4fe565ec72a2cc4f690e6
hurricane1026 <hurricane1026@gmail.com>
2024-01-06 16:24:36 +0800

    make AlignUpTo<> support any alignment, it was very faster than AlignUp
    re-format

--- align/align.hpp
+++ align/align.hpp
@@ -35,15 +35,14 @@
 
 namespace stdb::memory::align {
 
-inline constexpr uint64_t kMaxAlignSize = 64;
+inline constexpr uint64_t kMinAlignSize = 8;  // 64 bits for modern cpu
 // Align to next 8 multiple
 template <uint64_t N>
 [[gnu::always_inline]] constexpr auto AlignUpTo(uint64_t n) noexcept -> uint64_t {
     // Align n to next multiple of N
     // (from <Hacker's Delight 2rd edition>,Chapter 3.)
     static_assert((N & (N - 1)) == 0, "AlignUpToN, N is power of 2");
-    static_assert(N > 2, "AlignUpToN, N is than 4");
-    static_assert(N < kMaxAlignSize, "AlignUpToN, N is more than 64");
+    static_assert(N >= kMinAlignSize, "AlignUpToN, N should be more than 8");  // alignt to 2/4 doesnt make sense
     return (n + N - 1) & static_cast<uint64_t>(-N);
 }
 
