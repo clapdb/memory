8092893fc1c00933006740a548b08656ac20184c
hurricane1026 <hurricane1026@gmail.com>
2024-11-29 15:05:18 +0800

    add copyright info to small_string.hpp

--- string/small_string.hpp
+++ string/small_string.hpp
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2020 Beijing Jinyi Data Technology Co., Ltd. All rights reserved.
+ *
+ * This document is the property of Beijing Jinyi Data Technology Co., Ltd.
+ * It is considered confidential and proprietary.
+ *
+ * This document may not be reproduced or transmitted in any form,
+ * in whole or in part, without the express written permission of
+ * Beijing Jinyi Data Technology Co., Ltd.
+ */
+
 
 
 #pragma once
